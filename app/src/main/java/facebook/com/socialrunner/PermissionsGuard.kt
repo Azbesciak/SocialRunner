58fd510627ad1d1be6eca9a3600a76e78bea7fc0
Jakub Tomczak <jakub.tomczak@live.com>
2018-05-31 14:23:26 +0200

    Add permission code.

--- app/src/main/java/facebook/com/socialrunner/PermissionsGuard.kt
+++ app/src/main/java/facebook/com/socialrunner/PermissionsGuard.kt
@@ -8,21 +8,21 @@ import android.support.v4.content.ContextCompat
 class PermissionsGuard(var activity : Activity){
     fun accuirePermisions()
     {
-        checkPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-        checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        checkPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSIONS_REQUEST_CODE)
+        checkPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_REQUEST_CODE)
 
     }
 
-    private fun checkPermission(permission : String){
+    private fun checkPermission(permission : String, permissionCode : Int){
         if (ContextCompat.checkSelfPermission(activity,permission) != PackageManager.PERMISSION_GRANTED) {
             // Permission is not granted
             if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
                 ActivityCompat.requestPermissions(activity,
-                        arrayOf(permission), LOCATION_PERMISSION_REQUEST_CODE)
+                        arrayOf(permission), permissionCode)
             } else {
                 // No explanation needed, we can request the permission.
                 ActivityCompat.requestPermissions(activity, arrayOf(permission),
-                        LOCATION_PERMISSION_REQUEST_CODE)
+                        permissionCode)
             }
         }
     }
